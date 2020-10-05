3be83ff0cf0f6a8f8f915e95aca04e893180dff2
AlexSousaAlvess <alexsousaalves317@gmail.com>
2020-10-05 18:34:34 -0300

    create entity Client

---
+++ backend/src/main/java/com/sds/desafio/one/entities/Client.java
@@ -0,0 +1,93 @@
+package com.sds.desafio.one.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double income;
+	private Instant birtDate;
+	private Integer children;
+
+	public Client() {
+		
+	}
+
+	public Client(String name, String cpf, Double income, Instant birtDate, Integer children) {
+		this.name = name;
+		this.cpf = cpf;
+		this.income = income;
+		this.birtDate = birtDate;
+		this.children = children;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+
+	public Instant getBirtDate() {
+		return birtDate;
+	}
+
+	public void setBirtDate(Instant birtDate) {
+		this.birtDate = birtDate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
